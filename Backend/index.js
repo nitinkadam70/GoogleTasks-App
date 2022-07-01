adc3f186f317e075f2a3522c10af567a80005bf2
nitinkadam70 <99539536+nitinkadam70@users.noreply.github.com>
2022-07-02 02:28:53 +0530

    Backend Completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,23 +1,28 @@
 const express = require("express");
 const connection = require("./db");
+const cors = require("cors");
 
 //Routes
-
+const authRouter = require("./routes/auth.routes");
+const taskRouter = require("./routes/task.routes")
 
 const app = express();
 
+//middlewares
 app.use(express.json());
-app.get("/", (req, res) => {
-    res.send("Homepage")
-})
+app.use(cors());
+
+app.use("/auth", authRouter);
+app.use("/user", taskRouter);
+
 
 app.listen(8000, async () => {
     try {
         await connection;
         console.log("Connected to DB successfully");
     }
-    catch (err) {
+    catch {
         console.log("Failed to connect DB")
     }
-    console.log("Server running on Port localhost:8000")
+    console.log("Server running on localhost:8000")
 })
--- Backend/routes/task.routes.js
+++ Backend/routes/task.routes.js
@@ -1,9 +1,27 @@
 const { Router } = require("express")
+const TaskModel = require("../models/Task");
 
 const taskRouter = Router();
 
-taskRouter.get("/task", (req, res) => {
-    res.send("Task page")
+taskRouter.get("/:userId/task", async (req, res) => {
+    let userId = req.params;
+    let task = await TaskModel.find(userId)
+    res.send(task)
+})
+
+taskRouter.post("/:userId/task", async (req, res) => {
+    const userId = req.params.userId; //here we get userId from params 
+    let payload = {
+        ...req.body,
+        userId
+    }
+    let task = await new TaskModel(payload);
+    task.save((err, success) => {
+        if (err) {
+            return res.status(500).send({ message: "something went wrong" });
+        }
+        return res.status(201).send(success) //success is whole payload that we have with userId
+    })
 })
 
 module.exports = taskRouter;
