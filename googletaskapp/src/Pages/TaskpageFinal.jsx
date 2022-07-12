8af5dd8be880c56fbea24d578d862395510cd216
nitinkadam70 <99539536+nitinkadam70@users.noreply.github.com>
2022-07-12 18:25:06 +0530

    upadate

--- googletaskapp/src/Pages/TaskpageFinal.jsx
+++ googletaskapp/src/Pages/TaskpageFinal.jsx
@@ -9,9 +9,8 @@ import {
     Button,
     Stack,
     useToast,
-    SkeletonCircle,
-    SkeletonText,
-    Spinner
+    Spinner,
+    HStack
 } from '@chakra-ui/react';
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
@@ -23,9 +22,10 @@ import { getTaskToken } from '../Redux/task/Get/action';
 import { postTaskAction } from '../Redux/task/post/action';
 import { DeleteIcon } from '@chakra-ui/icons';
 import EditTask from '../Components/EditTask'
+import Errorpage from '../Components/Error';
 
 const backgrounds = [
-    `url("data:image/svg+xml, %3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'560\' height=\'185\' viewBox=\'0 0 560 185\' fill=\'none\'%3E%3Cellipse cx=\'102.633\' cy=\'61.0737\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23ED64A6\' /%3E%3Cellipse cx=\'399.573\' cy=\'123.926\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23F56565\' /%3E%3Cellipse cx=\'366.192\' cy=\'73.2292\' rx=\'193.808\' ry=\'73.2292\' fill=\'%2338B2AC\' /%3E%3Cellipse cx=\'222.705\' cy=\'110.585\' rx=\'193.808\' ry=\'73.2292\' fill=\'%23ED8936\' /%3E%3C/svg%3E")`,
+    `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='102.633' cy='61.0737' rx='102.633' ry='61.0737' fill='%23ED64A6' /%3E%3Cellipse cx='399.573' cy='123.926' rx='102.633' ry='61.0737' fill='%23F56565' /%3E%3Cellipse cx='366.192' cy='73.2292' rx='193.808' ry='73.2292' fill='%2338B2AC' /%3E%3Cellipse cx='222.705' cy='110.585' rx='193.808' ry='73.2292' fill='%23ED8936' /%3E%3C/svg%3E")`,
     `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ED8936'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%2348BB78'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%230BC5EA'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%23ED64A6'/%3E%3C/svg%3E")`,
     `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='102.633' cy='61.0737' rx='102.633' ry='61.0737' fill='%23ED8936'/%3E%3Cellipse cx='399.573' cy='123.926' rx='102.633' ry='61.0737' fill='%2348BB78'/%3E%3Cellipse cx='366.192' cy='73.2292' rx='193.808' ry='73.2292' fill='%230BC5EA'/%3E%3Cellipse cx='222.705' cy='110.585' rx='193.808' ry='73.2292' fill='%23ED64A6'/%3E%3C/svg%3E")`,
     `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ECC94B'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%239F7AEA'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%234299E1'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%2348BB78'/%3E%3C/svg%3E")`,
@@ -113,16 +113,18 @@ function TestimonialCard(props) {
                 direction={'column'}
                 textAlign={'left'}
                 justifyContent={'space-between'}>
-                <chakra.p fontFamily={'Work Sans'} fontWeight={'bold'} fontSize={14}>
-                    <chakra.span
-                        fontFamily={'Inter'}
-                        fontWeight={'medium'}
-                        color={'gray.500'}>
-                        {index + 1}. {" "}
-                    </chakra.span>
-                    {title}
+                <HStack>
+                    <chakra.p w={'134px'} fontFamily={'Work Sans'} fontWeight={'bold'} fontSize={14} >
+                        <chakra.span
+                            fontFamily={'Inter'}
+                            fontWeight={'medium'}
+                            color={'gray.500'}>
+                            {index + 1}. {" "}
+                        </chakra.span>
+                        {title}
+                    </chakra.p>
                     <EditTask _id={_id} />
-                </chakra.p>
+                </HStack>
                 <Button bg={status ? "whatsapp.100" : "red.100"} onClick={() => handleEdit(_id, status)}>{status ? "Completed" : "Incompleted"}</Button>
                 <Button colorScheme='red' rightIcon={<DeleteIcon />} onClick={() => handleDelete(_id)}>Delete</Button>
 
@@ -146,7 +148,7 @@ export default function TaskpageFinal() {
         if (!userid) {
             navigate("/login");
         }
-    }, [dispatch, task?.length, userid])
+    }, [dispatch, task?.length, userid, navigate])
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -208,7 +210,7 @@ export default function TaskpageFinal() {
                         size={'xl'}
                     />
 
-                    : task.map((cardInfo, index) => (
+                    : error ? <Errorpage /> : task.map((cardInfo, index) => (
                         <TestimonialCard key={index} {...cardInfo} index={index} />
                     ))}
             </SimpleGrid>
